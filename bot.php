fefb100b61b5f63a9abafdaa552969b73413c905
jsbsh666 <102283782+jsbsh666@users.noreply.github.com>
2022-03-26 17:08:32 +0900

    Update bot.php

--- bot.php
+++ bot.php
@@ -20,7 +20,7 @@ if(strpos($message, "!bin") === 0){
     $bin = substr($message, 5);
     $curl = curl_init();
     curl_setopt_array($curl, [
-    CURLOPT_URL => "https://binwebckbot.vercel.app/api/".$bin,
+    CURLOPT_URL => "https://ckbot.vercel.app/api/".$bin,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_ENCODING => "",
